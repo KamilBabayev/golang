89212740ac242a0c10da630dcf4fa70b3dbf6c02
Kamil Babayev <kamilyunis@gmail.com>
2016-09-28 23:24:20 +0400

    second

--- second.go
+++ second.go
@@ -6,5 +6,6 @@ import "fmt"
 // Integer types are uint8,uint16,uint32,uint64 and int8,int16,int32,int64 (2 alias types : byte(8bit) - uint8, rune - int32)
 // uint is unsigned integers which conrains positive numbers or zero. floar point is 1.23, 33.5, 0.23.
 
-
-
+func main() {
+	fmt.Println("1 + 1 =", 1 + 1)
+}
