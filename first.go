5b86214f6206a83ba28d77501df05601a7e2c711
Kamil Babayev <kamilyunis@gmail.com>
2016-09-28 11:00:13 +0400

    First go program

---
+++ first.go
@@ -0,0 +1,7 @@
+package main	#Package declaration, libraries
+
+import "fmt"	#we import fmt package (formatting for input, output) files in fmt package starts with fmt.
+
+func main() {
+	fmt.Println("Hello World!! ")
+}
