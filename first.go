92208bffbc905287ee7efa5e0bb28fa38865afda
Kamil Babayev <kamilyunis@gmail.com>
2016-09-28 11:05:25 +0400

    first

--- first.go
+++ first.go
@@ -1,6 +1,10 @@
 package main	//Package declaration, libraries
 // Comment line, lines starts or contains with // is comments
-
+/*
+This is multiline comment form,
+Go also supports this type, 
+but, I miss for python :)
+*/
 import "fmt"	//we import fmt package (formatting for input, output) files in fmt package starts with fmt.
 
 func main() {
