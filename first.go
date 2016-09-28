c66205195c0f77e355e36f5ea01df1bd87e0d69e
Kamil Babayev <kamilyunis@gmail.com>
2016-09-28 11:22:54 +0400

    first

--- first.go
+++ first.go
@@ -7,6 +7,22 @@ but, I miss for python :)
 */
 import "fmt"	//we import fmt package (formatting for input, output) files in fmt package starts with fmt.
 
-func main() {
-	fmt.Println("Hello World!! ")
+/*
+Function declaration, every function starts with func keywork, func name and arguments
+main function is special and it is called when program executes, that is why we see print result.
+*/
+func main() {		
+	fmt.Println("Hello World!! ")   // Println  takes one argument and executes it.
 }
+
+func new() {
+	fmt.Println("This line will not print, because it is just declaration")
+}
+
+
+
+
+
+// go run first.go		- this is just for running script
+// go build first.go		- this will compile script and create binary file
+// go doc frm  or go doc frm Println	 -  this will print help menu about frt and its internal method Println
