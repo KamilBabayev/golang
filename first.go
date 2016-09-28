2324e8e987a48ce448975ea865db27e71a39308a
Kamil Babayev <kamilyunis@gmail.com>
2016-09-28 11:03:35 +0400

    first

--- first.go
+++ first.go
@@ -1,6 +1,7 @@
-package main	#Package declaration, libraries
+package main	//Package declaration, libraries
+// Comment line, lines starts or contains with // is comments
 
-import "fmt"	#we import fmt package (formatting for input, output) files in fmt package starts with fmt.
+import "fmt"	//we import fmt package (formatting for input, output) files in fmt package starts with fmt.
 
 func main() {
 	fmt.Println("Hello World!! ")
