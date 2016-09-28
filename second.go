f33e3405e7d689a2ed4756c3b075c16cbd4337d7
Kamil Babayev <kamilyunis@gmail.com>
2016-09-28 11:50:31 +0400

    second

---
+++ second.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+// Go is statically typed lang, variables have types and that type can not change.
+// GO has built-in data types. integers and float-poit numbers
+// Integer types are uint8,uint16,uint32,uint64 and int8,int16,int32,int64 (2 alias types : byte(8bit) - uint8, rune - int32)
+// uint is unsigned integers which conrains positive numbers or zero. floar point is 1.23, 33.5, 0.23.
+
+
+
